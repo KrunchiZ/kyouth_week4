f28ea54b5e4d57f7e9c0082bf067540a7cebc729
kchiang <kaizhi3006@gmail.com>
2026-07-01 03:48:43 +0800

    refactor(sql): remove content_hash from response content

--- sql/fetch_all_cards.sql
+++ sql/fetch_all_cards.sql
@@ -1,3 +1,17 @@
-SELECT * FROM credit_cards
+SELECT
+    card_title,
+    bank,
+    cashback,
+    petrol,
+    rewards,
+    travel,
+    premium_perks,
+    balance_transfer,
+    easy_payment_plan,
+    fees,
+    requirements,
+    features,
+    min_annual_income
+FROM credit_cards
 LIMIT :limit
 OFFSET :offset
--- sql/fetch_card_by_title.sql
+++ sql/fetch_card_by_title.sql
@@ -1,2 +1,16 @@
-SELECT * FROM credit_cards
+SELECT
+    card_title,
+    bank,
+    cashback,
+    petrol,
+    rewards,
+    travel,
+    premium_perks,
+    balance_transfer,
+    easy_payment_plan,
+    fees,
+    requirements,
+    features,
+    min_annual_income
+FROM credit_cards
 WHERE card_title = :card_title
